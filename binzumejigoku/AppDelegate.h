cf9143ea447f3da9f912b7c2062bcbcfbc379a0d
Shizuo Ichitake <developer.tk74@gmail.com>
2016-07-29 22:01:50 +0900

    Initial Commit

---
+++ binzumejigoku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  binzumejigoku
+//
+//  Created by Shizuo Ichitake on 2016/07/29.
+//  Copyright © 2016年 1-Take. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ binzumejigoku/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  binzumejigoku
+//
+//  Created by Shizuo Ichitake on 2016/07/29.
+//  Copyright © 2016年 1-Take. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
